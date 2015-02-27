9bb1292d070891864da4feb9008c4da5560abec3
Joxit <Joxit972@gmail.com>
2015-02-27 03:56:59 +0100

    Add Projet Programmation Fonctionnelle Avancée

---
+++ M2/PFA/symbioz.ml
@@ -0,0 +1,509 @@
+(*******************************************************************************
+ *                                                                             *
+ * Devoir de Programmation Fonctionnelle Avancée                               *
+ *         Projet : Symbioz                                                    *
+ * Auteur Jones Magloire (11000369)                                            *
+ *                                                                             *
+ *******************************************************************************)
+
+(* 15:17 *)
+(*******************************************************************************
+ *    5  Fonctions globales                                                    *
+ *******************************************************************************)
+
+(*************************** 5.1 Aléatoire, options ****************************)
+
+(* Initialisation du générateur aléatoire *)
+let _ = Random.self_init ();;
+
+
+(********************************* 5.2 Listes **********************************)
+  
+(* 1/ Insert un élément à une position. Si la position est trop grande on 
+ * l'insert a la fin de la liste. On commence à la position 0. *)
+let rec insert_at_pos elt list pos = match list with
+  | [] -> [elt]
+  | hd::tl when pos = 0 -> elt::hd::tl
+  | hd::tl -> hd::insert_at_pos elt tl (pos-1);;
+
+(* 2/ Insert un élément à une position aléatoire. *)
+let insert_at_random elt list = insert_at_pos elt list (Random.int (List.length list));;
+
+(* 3/ Melange une liste. *)
+let rec shuffle list = match list with
+  | hd::[] -> [hd]
+  | hd::tl -> insert_at_random hd (shuffle tl)
+  (* On le met en derniere position car on ne peut pas melanger *)
+  | [] -> [];;
+
+(* 4/ Renvoie un élément aleatoire de la liste typé en 'a option *)
+let random_get list = match list with
+  | [] -> None
+  | _ -> Some (List.nth list (Random.int (List.length list)));;
+
+(* 5/ Renvoie la liste des éléments qui ne sont pas None *)
+let rec clean_list list = match list with
+  | [] -> []
+  | None::tl -> clean_list tl
+  | (Some hd)::tl -> hd::clean_list tl;;
+
+(****************************** 5.3 Âge et sexe  *******************************)
+
+(* 6/ Les types enumérés *)
+type sexe =
+    Masculin
+  | Feminin;;
+
+type age =
+    Bebe
+  | Enfant
+  | Adulte;;
+
+(* 7/ Renvoie une valeur au hasard *)
+let random_sexe () = match (Random.int 2) with
+  | 0 -> Masculin
+  | _ -> Feminin;;
+  
+let random_age () = match (Random.int 3) with
+  | 0 -> Bebe
+  | 1 -> Enfant
+  | _ -> Adulte;;
+
+(* 8/ Transforme les valeurs en chaine de caractères *)
+let string_sexe sexe = match sexe with
+  | Masculin -> "Masculin"
+  | Feminin -> "Feminin";;
+  
+let string_age age = match age with
+  | Bebe -> "Bebe"
+  | Enfant -> "Enfant"
+  | Adulte -> "Adulte";;
+
+(*******************************************************************************
+ *    6  La planète Symbioz                                                    *
+ *******************************************************************************)
+
+(******************************** 6.1 Signature ********************************)
+
+(* 9/ Signature de module PLANETE *)
+module type PLANETE =
+  sig
+    type pos
+    val ouest : pos -> pos
+    val est : pos -> pos
+    val nord : pos -> pos
+    val sud : pos -> pos
+    val random_pos : unit -> pos
+    val at_pos : ('a -> pos) -> pos -> 'a list -> 'a list
+    val sort_by_pos : ('a -> pos) -> 'a list -> 'a list list
+    val display : (int -> int -> unit) -> pos -> unit
+    val clear : unit -> unit
+  end;;
+
+(********************************** 6.2 Module *********************************)
+
+(* 10/ Implementation du module Symbioz *)
+let size_x = 10;;
+let size_y = 10;;
+  
+module Symbioz:PLANETE =
+  struct
+    type pos = int * int
+    let ouest (x, y) =
+      let x2 =
+	if x = 0 then
+	  (size_x - 1)
+	else
+	  (x - 1)
+      in (x2, y)
+    let est (x, y) =
+      let x2 =
+	if (x + 1) = size_x then
+	  0
+	else
+	  (x + 1)
+      in (x2, y)
+    let nord (x, y) =
+      let y2 = 
+	if y = 0 then
+	  (size_x - 1)
+	else (y - 1)
+      in (x, y2)
+    let sud (x, y) =
+      let y2 = 
+	if (y + 1) = size_y then
+	  0
+	else
+	  (y + 1)
+      in (x, y2)
+    let random_pos () = (Random.int size_x, Random.int size_y)
+    let rec at_pos get_a_pos pos list = match list with
+      | [] -> []
+      | hd::tl -> let hd_pos = get_a_pos hd in
+		  if (fst hd_pos) = (fst pos) && (snd hd_pos) = (snd pos)
+		  then
+		    hd::(at_pos get_a_pos pos tl)
+		  else
+		    at_pos get_a_pos pos tl
+    let sort_by_pos get_a_pos list =
+      let rec sort_x x =
+	if x = size_x then
+	  []
+	else 
+	  let rec sort_y y =
+	    if y = size_y  then
+	      []
+	    else
+	      (at_pos get_a_pos (x, y) list)::sort_y (y + 1)
+	  in (sort_y 0)@(sort_x (x + 1))
+      in sort_x 0
+    let display f (x,y) = f x y
+    let clear () = ()
+  end;;
+
+(* 17:38 *) 
+(*******************************************************************************
+ *    7  Génétique élémentaire                                                 *
+ *******************************************************************************)
+
+(* 18:14 - 18:33 *)
+(********************************* 7.1 Signature *******************************)
+
+(* 11/ Signature INDIVIDU *)
+module type INDIVIDU =
+  sig
+    type pos
+    type individu
+    type t = individu
+    val last_id : int ref
+    val equals : individu -> individu -> bool
+    val create : unit -> individu
+    val get_pos : individu -> pos
+    val get_sexe : individu -> sexe
+    val get_age : individu -> age
+    val manger : int -> individu -> individu option
+    val bouger : (pos -> int) -> individu -> individu
+    val reproduire : int -> individu -> individu -> individu list
+    val vieillir : individu -> individu option
+  end;;
+
+(* 23:32 *)
+(* 12/ Signature MAKE_INDIVIDU *)
+module type MAKE_INDIVIDU =
+  functor (P:PLANETE) ->INDIVIDU with type pos = P.pos;;
+
+(********************************** 7.2 Zherbs *********************************)
+
+(* 13/ Mudule Make_Zherb *)
+module Make_Zherb:MAKE_INDIVIDU = 
+  functor (P:PLANETE) ->
+    struct
+      type pos = P.pos
+      type individu = {id:int; age:age; pos:pos}
+      type t = individu
+      let last_id = ref 0
+      let equals individu1 individu2 = individu1.id = individu2.id
+      let create () =  last_id := !last_id + 1 ;
+	{id = !last_id; age = random_age (); pos = P.random_pos ()}
+      let get_pos individu = individu.pos
+      let get_sexe individu = failwith "Il n'a pas de sexe"
+      let get_age individu = individu.age
+      let manger i individu = Some individu
+      let bouger f individu = individu
+      let reproduire nb_zherbs mere pere =
+	let rec create_children n = match n with
+	  | 0 -> []
+	  | x ->
+	    let rnd_case = Random.int 100
+	    in let child = create ()
+	       in let new_pos =
+		    if rnd_case < 60 then
+		      mere.pos
+		    else if rnd_case < 70 then
+		      P.ouest mere.pos
+		    else if rnd_case < 80 then
+		      P.est mere.pos
+		    else if rnd_case < 90 then
+		      P.nord mere.pos
+		    else
+		      P.sud mere.pos
+		  in {child with
+		    age = Bebe;
+		    pos = new_pos}::create_children (x - 1)
+	in
+	if mere.age = Adulte then
+	  create_children (Random.int (nb_zherbs + 1))
+	else 
+	  []
+      let vieillir individu = 
+	if get_age individu = Bebe then
+	  Some {individu with age = Enfant}
+	else if get_age individu = Enfant then
+	  Some {individu with age = Adulte}
+	else
+	  None
+    end;;
+
+(********************************** 7.3 Krapits ********************************)
+(* 1:00 *)
+(* 14/ Mudule Make_Krapit *)
+let krapit_pv_max = 6;;
+let krapit_pv_consum = 1;;
+let krapit_seuil_faim = 3;;
+module Make_Krapit:MAKE_INDIVIDU = functor (P:PLANETE) ->
+  struct
+    type pos = P.pos
+    type individu = {id:int; sexe:sexe; age:(age * int); pos:pos; pv:int}
+    type t = individu
+    let last_id = ref 0
+    let equals individu1 individu2 = individu1.id = individu2.id
+    let create () =  last_id := !last_id + 1 ;
+		     {id = !last_id;
+		      sexe = random_sexe ();
+		      (* Comme l'age est aléatoire, on ne sait pas si il sera 
+adulte ou non, on initialise donc aussi le second champs *)
+		      age = (random_age (), (Random.int 4) + 1);
+		      pos = P.random_pos ();
+		      pv = krapit_pv_max}
+    let get_pos individu = individu.pos
+    let get_sexe individu = individu.sexe
+    let get_age individu = fst individu.age
+    let manger qte individu = 
+      if qte > 0 then 
+	Some {individu with pv = krapit_pv_max}
+      else if  individu.pv > krapit_pv_consum then 
+	(* Si on a plus de pv que ce qu'on va perdre il survit *)
+	Some {individu with  pv = individu.pv - krapit_pv_consum}
+      else 
+	(* Sinon il meurt *)
+	None
+    let bouger f individu = 
+      if individu.pv > krapit_seuil_faim then
+	individu
+      else
+	let rnd = Random.int 4
+	in let new_pos =
+	     if rnd = 0 then
+	       P.ouest individu.pos
+	     else if rnd = 1 then
+	       P.est individu.pos
+	     else if rnd = 2 then
+	       P.nord individu.pos
+	     else
+	       P.sud individu.pos
+	   in {individu with pos = new_pos}
+    let reproduire nb_zherbs mere pere =
+     	let rec create_children n = match n with
+	  | 0 -> []
+	  | x ->
+	     let rnd_case = Random.int 100
+	     in let child = create ()
+		in let new_pos =
+		     if rnd_case < 20 then
+		       child.pos
+		     else if rnd_case < 40 then
+		       P.ouest mere.pos
+		     else if rnd_case < 60 then
+		       P.est mere.pos
+		     else if rnd_case < 80 then
+		       P.nord mere.pos
+		     else
+		       P.sud child.pos
+		   in {child with 
+		     age = (Bebe, 0);
+		     pos = new_pos}::create_children (x - 1)
+	in if get_age mere = Adulte && get_age pere = Adulte then
+	     create_children ((Random.int 5) + 1)
+	   else []
+    let vieillir individu = 
+      if snd individu.age > 1 then (* on reduit l'esperance de vie dans tous les cas *)
+	Some {individu with age = (get_age individu, snd individu.age - 1)}
+      else if get_age individu = Bebe then (* Sinon on change d'age *)
+	Some {individu with age = (Enfant, 0)}
+      else if get_age individu = Enfant then
+	Some {individu with age = (Adulte, (Random.int 4) + 1)} (* entre 1 et 4 *)
+      else
+	None
+  end;;
+(* 2:00 *)
+(********************************** 7.4 Kroguls ********************************)
+
+(* 21:57 *)
+(* 15/ Mudule Make_Kroguls *)
+let krogul_pv_max = 10;;
+let krogul_pv_consum = -2;;
+let krogul_seuil_faim = 6;;
+let krogul_gain_faim = 5;;
+let krogul_affame = 4;;
+module Make_Kroguls:MAKE_INDIVIDU = functor (P:PLANETE) ->
+  struct
+    type pos = P.pos
+    type individu = {id:int; sexe:sexe; age:(age * int); pos:pos; pv:int}
+    type t = individu
+    let last_id = ref 0
+    let equals individu1 individu2 = individu1.id = individu2.id
+    let create () =  
+      last_id := !last_id + 1 ; 
+      let rnd_age = random_age ()
+      in let age = 
+	   if rnd_age = Adulte then
+	     (rnd_age, (Random.int 4) + 2)  (* 2 a 5 tours adulte *)
+	   else
+	     (rnd_age, 2)
+	 in {id = !last_id;
+	     sexe = random_sexe ();
+	     age = age;
+	     pos = P.random_pos ();
+	     pv = krogul_pv_max}
+    let get_pos individu = individu.pos
+    let get_sexe individu = failwith "Il n'a pas de sexe"
+    let get_age individu = fst individu.age
+    let manger qte individu = 
+      if qte > 0 && individu.pv <= krogul_seuil_faim then 
+	Some {individu with 
+	  pv = individu.pv + (krogul_gain_faim * qte)} (* le nombre de krapits manges *)
+      else if  individu.pv > krogul_pv_consum then 
+	(* Si on a plus de pv que ce qu'on va perdre il survit *)
+	Some {individu with  pv = individu.pv - krogul_pv_consum}
+      else 
+	(* Sinon il meurt *)
+	None
+    let bouger f individu = 
+      if individu.pv > krogul_affame then
+	individu
+      else
+	let rnd = f individu.pos
+	in let new_pos =
+	     if rnd = 0 then
+	       P.ouest individu.pos
+	     else if rnd = 1 then
+	       P.est individu.pos
+	     else if rnd = 2 then
+	       P.nord individu.pos
+	     else
+	       P.sud individu.pos
+	   in {individu with pos = new_pos}
+    let reproduire nb_zherbs mere pere =
+     	let rec create_children n = match n with
+	  | 0 -> []
+	  | x ->
+	     let rnd_case = Random.int 100
+	     in let child = create ()
+		in let new_pos =
+		     if rnd_case < 80 then
+		       child.pos
+		     else if rnd_case < 85 then
+		       P.ouest mere.pos
+		     else if rnd_case < 90 then
+		       P.est mere.pos
+		     else if rnd_case < 95 then
+		       P.nord mere.pos
+		     else
+		       P.sud child.pos
+		   in {child with 
+		     age = (Bebe, 0);
+		     pos = new_pos}::create_children (x - 1)
+	in if get_age mere = Adulte && get_age pere = Adulte then
+	     create_children (Random.int 3)
+	   else []
+    let vieillir individu = 
+      if snd individu.age > 1 then (* on reduit l'esperance de vie dans tous les cas *)
+	Some {individu with age = (get_age individu, snd individu.age - 1)}
+      else if get_age individu = Bebe then (* Sinon on change d'age *)
+	Some {individu with age = (Enfant, 2)}
+      else if get_age individu = Enfant then
+	Some {individu with age = (Adulte, (Random.int 4) + 2)} (* 2 a 5 tours *)
+      else (* Quand tout est faux il meurt *)
+	None
+  end;;  
+(* 22:55 *)
+
+
+(*******************************************************************************
+ *    8  Population                                                            *
+ *******************************************************************************)
+
+(********************************* 8.1 Signatures *******************************)
+
+(* 16/ Signature Population *)
+(* 23:15 *)
+module type POPULATION =
+  sig
+    type pos
+    type population
+    type individu
+    type nourriture
+    type elt = individu
+    type t = population
+    val create : int -> population
+    val random_ind : population ->  individu option
+    val sous_pop : pos -> population -> population
+    val tuer_ind : individu -> population -> population
+    val map : ('a -> 'b) -> 'a list -> 'b list
+    val iter : ('a -> unit) -> 'a list -> unit
+    val reduce : (individu -> 'a -> 'a) -> population -> 'a -> 'a
+    val vieillissement : population -> population
+    val reproduction : population -> population
+    val mouvement : nourriture -> population -> population
+    val nourriture : nourriture -> population -> (population * nourriture)
+    val affichage : population -> unit
+  end;;
+
+(* 23:36 *)
+
+module type MAKE_PLANTES = 
+  functor (P:PLANETE) -> 
+    functor (MI:MAKE_INDIVIDU) -> 
+     POPULATION with type pos = P.pos
+		and type nourriture = unit;;
+(* 23:57 *)
+module type MAKE_ANIMAUX = 
+  functor (P:PLANETE) ->
+    functor (PROIES:POPULATION with type pos = P.pos) -> 
+      functor (MI:MAKE_INDIVIDU) -> 
+	POPULATION with type pos = P.pos
+		   and type nourriture = PROIES.individu;;
+(* 0:02 *) 
+
+(********************************* 8.2 Zherbs *********************************)
+(* 1:28 *)
+module Make_Zherbs =
+  functor (P:PLANETE) ->
+    functor (MI:MAKE_INDIVIDU) -> 
+      struct 
+	type pos = P.pos
+	type population = individu list
+	type individu = MI.individu
+	type nourriture = unit
+	type elt = individu
+	type t = population
+	let create n = map MI.create (Array.to_list (Array.make n ()))
+	let random_ind  population = random_get population
+	let sous_pop pos population = P.at_pos MI.get_pos population
+	let tuer_ind individu population = 
+	  List.filter (function ind -> !MI.equals individu ind) population
+	let map f list = List.map f list
+	let iter f population = List.iter f population
+	let reduce f population a = List.fold_right f population a
+	let vieillissement population = clean_list (map MI.vieillir population)
+	let reproduction  population = clean_list (map MI.reproduire population)
+	let mouvement nourriture population = population
+	let nourriture 
+	    nourriture  population = (population, nourriture)
+	let affichage population = ()
+  end:MAKE_PLANTES;;
+
+
+module Make_Game =
+  functor (P:PLANETE) ->
+    functor (Plantes:POPULATION with type pos = P.pos
+				and type nourriture = unit) ->
+      functor (Herbivores:POPULATION with type pos = P.pos
+				and type nourriture = Plantes.population) ->
+	functor (Carnivores:POPULATION with type pos = P.pos
+				and type nourriture = Herbivores.population) ->
+struct
+  
+end;;
+
+(* 3:21 *)
